d8a311ea53e994d2383e43456fc9b5b203c40fd8
ktottenCU <43186631+ktottenCU@users.noreply.github.com>
2018-09-16 14:07:47 -0400

    Added Initial Tests

---
+++ src/TestSuite.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+import junit.*;
+
+//Class for all the tests
+public class TestSuite{
+  
+  @Test
+  public void testInitialCards() {
+     
+  }
+  
+  @Test
+  public void testAiInitialCards() {
+     
+  }
+  
+}

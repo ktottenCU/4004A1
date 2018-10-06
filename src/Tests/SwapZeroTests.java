90aa55e6789fd30516cd1f592bedb15308c3adc3
ktottenCU <43186631+ktottenCU@users.noreply.github.com>
2018-10-06 13:39:33 -0400

    Test Commit: Added 12 tests for detecting and not swapping on RoyalFlsuh, StraightFlush, FourOfAKind, FullHouse, Flush, and Straight.

--- src/Tests/SwapZeroTests.java
+++ src/Tests/SwapZeroTests.java
@@ -10,9 +10,65 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class SwapZeroTests {
+  //Detecting Royal Flush, Straight Flush, Four of a Kind, Full House, Flush and Straight
   
   @Test
-  public void baseTest() {
+  public void inOrderRoyalFlushTest() {
+    fail();
+  }
+  
+  @Test
+  public void outOrderRoyalFlushTest() {
+    fail();
+  }
+  
+  @Test
+  public void inOrderStraightFlushTest() {
+    fail();
+  }
+  
+  @Test
+  public void outOrderStraightFlushTest() {
+    fail();
+  }
+  
+  @Test
+  public void inOrderFourKindTest() {
+    fail();
+  }
+  
+  @Test
+  public void outOrderFourKindTest() {
+    fail();
+  }
+  
+  @Test
+  public void inOrderFullHouseTest() {
+    fail();
+  }
+  
+  @Test
+  public void outOrderFullHouseTest() {
+    fail();
+  }
+  
+  @Test
+  public void inOrderFlushTest() {
+    fail();
+  }
+  
+  @Test
+  public void outOrderFlushTest() {
+    fail();
+  }
+  
+  @Test
+  public void inOrderStraightTest() {
+    fail();
+  }
+  
+  @Test
+  public void outOrderStraightTest() {
     fail();
   }
 }

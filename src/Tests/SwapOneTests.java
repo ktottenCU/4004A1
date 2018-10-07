cbf52db890d96b34735e670c2a39b7d4d49d6834
ktottenCU <43186631+ktottenCU@users.noreply.github.com>
2018-10-07 14:30:26 -0400

    Test Commit: Added test for flush one-swap

--- src/Tests/SwapOneTests.java
+++ src/Tests/SwapOneTests.java
@@ -47,7 +47,18 @@ public class SwapOneTests {
   
   @Test
   public void flushOneSwapTest() {
-    
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 8);
+    assertEquals(testGame.aiHand.hand.get(0), "S6");
+    assertEquals(testGame.aiHand.hand.get(1), "S9");
+    assertEquals(testGame.aiHand.hand.get(2), "S8");
+    assertEquals(testGame.aiHand.hand.get(3), "SJ");
+    assertEquals(testGame.aiHand.hand.get(4), "CK");
+    testGame.processBehaviour();
+    assertEquals(testGame.aiHand.hand.get(0), "S6");
+    assertEquals(testGame.aiHand.hand.get(1), "S9");
+    assertEquals(testGame.aiHand.hand.get(2), "S8");
+    assertEquals(testGame.aiHand.hand.get(3), "SJ");
+    assertEquals(testGame.aiHand.hand.get(4), "SQ");
   }
   
   @Test

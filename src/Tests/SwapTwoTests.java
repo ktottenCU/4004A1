e59976343a894003a9d40eab26d04b8fd04c2e6e
ktottenCU <43186631+ktottenCU@users.noreply.github.com>
2018-10-07 18:49:08 -0400

    Test Commit: Created the test for swapping 2 cards if 3 of a kind is present and added line to cardfile

--- src/Tests/SwapTwoTests.java
+++ src/Tests/SwapTwoTests.java
@@ -5,14 +5,20 @@ package Tests;
 //
 ////////////////////
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import Poker.PokerGame;
+
 public class SwapTwoTests {
 
   @Test
-  public void baseTest() {
-    fail();
+  public void threeKindSwapTwoTest() {
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 10);
+    assertEquals(testGame.aiHand.hasThreeKind(), true);
+    testGame.processBehaviour();
+    assertEquals(testGame.aiHand.hasFourOfAKind(), true);
   }
 }

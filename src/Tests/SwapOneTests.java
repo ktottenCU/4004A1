af379b1d7c942f953e6b25547fd2624125428515
ktottenCU <43186631+ktottenCU@users.noreply.github.com>
2018-10-07 17:17:57 -0400

    Test Commit: Added line in cardfile, and added test for swapping 1 off straight

--- src/Tests/SwapOneTests.java
+++ src/Tests/SwapOneTests.java
@@ -38,22 +38,17 @@ public class SwapOneTests {
   @Test
   public void flushOneSwapTest() {
     PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 8);
-    assertEquals(testGame.aiHand.hand.get(0), "S6");
-    assertEquals(testGame.aiHand.hand.get(1), "S9");
-    assertEquals(testGame.aiHand.hand.get(2), "S8");
-    assertEquals(testGame.aiHand.hand.get(3), "SJ");
-    assertEquals(testGame.aiHand.hand.get(4), "CK");
+    assertEquals(testGame.aiHand.hasFlush(), false);
     testGame.processBehaviour();
-    assertEquals(testGame.aiHand.hand.get(0), "S6");
-    assertEquals(testGame.aiHand.hand.get(1), "S9");
-    assertEquals(testGame.aiHand.hand.get(2), "S8");
-    assertEquals(testGame.aiHand.hand.get(3), "SJ");
-    assertEquals(testGame.aiHand.hand.get(4), "SQ");
+    assertEquals(testGame.aiHand.hasFlush(), true);
   }
   
   @Test
   public void straightOneSwapTest() {
-    fail();
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 9);
+    assertEquals(testGame.aiHand.hasStraight(), false);
+    testGame.processBehaviour();
+    assertEquals(testGame.aiHand.hasStraight(), true);
   }
   
 }

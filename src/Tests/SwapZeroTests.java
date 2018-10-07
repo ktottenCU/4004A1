ac5fbec989ad56d405e7e18a8c97a9447bbe8c9c
ktottenCU <43186631+ktottenCU@users.noreply.github.com>
2018-10-06 21:32:54 -0400

    Test Commit: Added tests for not swapping any cards if the big hands are present

--- src/Tests/SwapZeroTests.java
+++ src/Tests/SwapZeroTests.java
@@ -5,40 +5,68 @@ package Tests;
 //
 ////////////////////
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
+
+import Poker.CardHand;
+import Poker.PokerGame;
 
 public class SwapZeroTests {
   //Detecting Royal Flush, Straight Flush, Four of a Kind, Full House, Flush and Straight
   
   @Test
   public void RoyalFlushTest() {
-    fail();
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 1);
+    CardHand pre = testGame.aiHand;
+    testGame.processBehaviour();
+    CardHand post = testGame.aiHand;
+    assertEquals((pre.hand.get(0) == post.hand.get(0) && pre.hand.get(1) == post.hand.get(1) && pre.hand.get(2) == post.hand.get(2) && pre.hand.get(3) == post.hand.get(3)&& pre.hand.get(4) == post.hand.get(4)), true);
   }
   
   @Test
-  public void StraightFlushTest() {
-    fail();
+  public void straightFlushTest() {
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 1);
+    CardHand pre = testGame.aiHand;
+    testGame.processBehaviour();
+    CardHand post = testGame.aiHand;
+    assertEquals((pre.hand.get(0) == post.hand.get(0) && pre.hand.get(1) == post.hand.get(1) && pre.hand.get(2) == post.hand.get(2) && pre.hand.get(3) == post.hand.get(3)&& pre.hand.get(4) == post.hand.get(4)), true);
   }
   
   @Test
-  public void FourKindTest() {
-    fail();
+  public void fourKindTest() {
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 3);
+    CardHand pre = testGame.aiHand;
+    testGame.processBehaviour();
+    CardHand post = testGame.aiHand;
+    assertEquals((pre.hand.get(0) == post.hand.get(0) && pre.hand.get(1) == post.hand.get(1) && pre.hand.get(2) == post.hand.get(2) && pre.hand.get(3) == post.hand.get(3)&& pre.hand.get(4) == post.hand.get(4)), true);
   }
   
   @Test
-  public void FullHouseTest() {
-    fail();
+  public void fullHouseTest() {
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 4);
+    CardHand pre = testGame.aiHand;
+    testGame.processBehaviour();
+    CardHand post = testGame.aiHand;
+    assertEquals((pre.hand.get(0) == post.hand.get(0) && pre.hand.get(1) == post.hand.get(1) && pre.hand.get(2) == post.hand.get(2) && pre.hand.get(3) == post.hand.get(3)&& pre.hand.get(4) == post.hand.get(4)), true);
   }
   
   @Test
-  public void FlushTest() {
-    fail();
+  public void flushTest() {
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 1);
+    CardHand pre = testGame.aiHand;
+    testGame.processBehaviour();
+    CardHand post = testGame.aiHand;
+    assertEquals((pre.hand.get(0) == post.hand.get(0) && pre.hand.get(1) == post.hand.get(1) && pre.hand.get(2) == post.hand.get(2) && pre.hand.get(3) == post.hand.get(3)&& pre.hand.get(4) == post.hand.get(4)), true);
   }
   
   @Test
-  public void StraightTest() {
-    fail();
+  public void straightTest() {
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 1);
+    CardHand pre = testGame.aiHand;
+    testGame.processBehaviour();
+    CardHand post = testGame.aiHand;
+    assertEquals((pre.hand.get(0) == post.hand.get(0) && pre.hand.get(1) == post.hand.get(1) && pre.hand.get(2) == post.hand.get(2) && pre.hand.get(3) == post.hand.get(3)&& pre.hand.get(4) == post.hand.get(4)), true);
   }
 }

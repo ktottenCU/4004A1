919e0e9416aae5877f218fc0c31db04dbf1ce0ef
ktottenCU <43186631+ktottenCU@users.noreply.github.com>
2018-10-08 13:51:15 -0400

    Test Commit: Added tests for twopair beating pair and high card

--- src/Tests/SwaplessWinTests.java
+++ src/Tests/SwaplessWinTests.java
@@ -14,4 +14,16 @@ public class SwaplessWinTests {
     PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 11);
     assertEquals(testGame.determineWinner(), testGame.opponentHand);
   }
+  
+  @Test
+  public void twoPairOverPair() {
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 12);
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);
+  }
+  @Test
+  public void twoPairOverHigh() {
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 13);
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);
+  }
+  
 }

0c17163880e21da099cddf63fa4b3638bbaf5761
ktottenCU <43186631+ktottenCU@users.noreply.github.com>
2018-10-07 20:11:20 -0400

    Test Commit: Added test to show that detection works on hand before and after swap, and isnt effected by swap.

--- src/Tests/DetectionTests.java
+++ src/Tests/DetectionTests.java
@@ -166,4 +166,12 @@ public class DetectionTests {
     assertEquals(testGame.opponentHand.hasStraight(), true);
   }
   
+  @Test
+  public void independenceDetectionTest() {
+    PokerGame testGame = new PokerGame("C:/Users/ktotten/workspace/4004A1/src/cardfile", 10);
+    assertEquals(testGame.aiHand.hasThreeKind(), true);
+    testGame.processBehaviour();
+    assertEquals(testGame.aiHand.hasFourOfAKind(), true);
+  }
+  
 }

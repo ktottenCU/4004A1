8a45a742de367ad629d4e0ef9d74bd20a13c245e
ktottenCU <43186631+ktottenCU@users.noreply.github.com>
2018-10-09 19:19:24 -0400

    Refactoring Commit: moved tests from swapless to swapped winTests suites.

--- src/Tests/SwappedWinTests.java
+++ src/Tests/SwappedWinTests.java
@@ -9,9 +9,89 @@ import Poker.PokerGame;
 
 public class SwappedWinTests {
 
+//Complex Wins here. This is where suit and highcard matters
   @Test
-  public void defaultTest() {
-    fail();
+  public void spadeRFvsHeartRF() {
+    PokerGame testGame = new PokerGame("src/cardfile", 56);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);
+  }
+  
+  @Test
+  public void heartRFvsDiamondRF() {
+    PokerGame testGame = new PokerGame("src/cardfile", 57);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);
+  }
+  
+  @Test
+  public void diamondRFvsClubRF() {
+    PokerGame testGame = new PokerGame("src/cardfile", 58);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);
+  }
+  
+  @Test
+  public void spadeRFvsClubRF() {
+    PokerGame testGame = new PokerGame("src/cardfile", 59);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.aiHand);
+  }
+  
+  @Test
+  public void spadeSFvsHeartSF(){
+    PokerGame testGame = new PokerGame("src/cardfile", 60);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);
+  }
+  
+  @Test
+  public void spadeSFvsHigherSF(){
+    PokerGame testGame = new PokerGame("src/cardfile", 61);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.aiHand);
+  }
+  
+  @Test
+  public void higherFourKind(){
+    PokerGame testGame = new PokerGame("src/cardfile", 62);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);  
+  }
+  
+  @Test
+  public void higherFullHouse(){
+    PokerGame testGame = new PokerGame("src/cardfile", 63);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);  
+  }
+  
+  @Test
+  public void higherStraight(){
+    PokerGame testGame = new PokerGame("src/cardfile", 64);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.aiHand);  
+  }
+  
+  @Test
+  public void higherStraightSuit(){
+    PokerGame testGame = new PokerGame("src/cardfile", 65);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);  
+  }
+  
+  @Test
+  public void highestCard(){
+    PokerGame testGame = new PokerGame("src/cardfile", 66);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);  
+  }
+  
+  @Test
+  public void highestCardSuit(){
+    PokerGame testGame = new PokerGame("src/cardfile", 67);
+    testGame.processBehaviour();
+    assertEquals(testGame.determineWinner(), testGame.opponentHand);  
   }
   
 }
